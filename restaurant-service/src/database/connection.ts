837a5d72942259559d4a7a6c679b70c3ce41b959
Stefano <stefano.notari1998@gmail.com>
2022-11-10 10:43:53 +0100

    moved restaurants-service to restaurant-service and updated mongodb connection

--- restaurant-service/src/database/connection.ts
+++ restaurant-service/src/database/connection.ts
@@ -1,11 +1,11 @@
 import mongoose, { ConnectOptions } from "mongoose";
 
 export async function connectToDatabase() {
-    const uri = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authsource=admin`
+    const uri = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`
     console.log(uri)
     const options = {
-        user: process.env.MONGO_USER,
-        pass: process.env.MONGO_PASSWORD,
+        /*user: process.env.MONGO_USER,
+        pass: process.env.MONGO_PASSWORD,*/
         keepAlive: true,
         useNewUrlParser: true,
         useUnifiedTopology: true,
